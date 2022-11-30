4b21c58d831886d912dcc6387cdbaa9af22c4d0d
benjamin-h-lee <95728536+benjamin-h-lee@users.noreply.github.com>
2022-11-30 14:54:22 +0900

    deleted everything

--- client/src/scrollToId.js
+++
@@ -1,9 +0,0 @@
-
-const scrollToId = (id) => {
-    const el = document.getElementById(id);
-    console.log("scrolling");
-    const elTop = window.scrollY + el.getBoundingClientRect().top;
-    window.scrollTo({ top: elTop, behavior: "smooth"});
-}
-
-export default scrollToId;
